83f3278e090152f872cab5fa14bee2e1876ef98c
jungwoo0719 <jungwoo07191@naver.com>
2025-12-07 23:28:53 +0900

    pls

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
-export const SPRING_API_BASE_URL = import.meta.env.VITE_SPRING_API_BASE_URL || "http://localhost:8080/api";
+export const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL || "https://devoted-miracle-production-abe9.up.railway.app";
+export const SPRING_API_BASE_URL = import.meta.env.VITE_SPRING_API_BASE_URL || "https://devoted-miracle-production-abe9.up.railway.app/api";
